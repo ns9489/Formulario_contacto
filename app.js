f1ea6e9d40cdf46c8eb14d5bc30da8bcf72db6d6
ns9489 <ns948966@gmail.com>
2024-08-03 12:15:19 -0500

    Se revisaron detalles y se modifico la interfaz del formulario

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const frmEmail = document.getElementById("frm-email")
 frmEmail.addEventListener("submit", sendEmail)
-const servicedId = "service_q4pof2z"
+const serviceId = "service_q4pof2z"
 const templateId = "template_h2vvyb8"
 const apikey = "e8FDx6_lKu3ZZ3p7A" 
 
 function sendEmail(event){
     event.preventDefault()
-    // emailjs.init(servicedId)
+    // emailjs.init(serviceId)
 
-    emailjs.sendForm(servicedId,templateId,frmEmail)
-    .then( rseult => Swal.fire ("Su mensaje se ha enviado con exito"))
+    emailjs.sendForm(serviceId,templateId,frmEmail)
+    .then( result => Swal.fire ("Su mensaje se ha enviado con exito"))
     .catch( error => {
         Swal.fire({
             icon: "error",
