facf32f31dcebd8484b5a4c4b5a8630616c75e5d
SAntiGB05 <santigbttobi@gmail.com>
2024-08-02 20:43:16 -0500

    El formulario funciona correctamente

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const frmEmail = document.getElementById("frm-email")
 frmEmail.addEventListener("submit", sendEmail)
-
 const servicedId = "service_q4pof2z"
 const templateId = "template_h2vvyb8"
 const apikey = "e8FDx6_lKu3ZZ3p7A" 
 
 function sendEmail(event){
-    event.preventDefaul()
-    emailjs.init(servicedId)
+    event.preventDefault()
+    // emailjs.init(servicedId)
 
-    emailjs.sendForm(servicedId,templateId,frmEmail.apikey)
+    emailjs.sendForm(servicedId,templateId,frmEmail)
     .then( rseult => Swal.fire ("Su mensaje se ha enviado con exito"))
     .catch( error => {
         Swal.fire({
             icon: "error",
             title: "Oops...",
-            Text: "No ha sido posible enviar el mensaje",
+            text: "No ha sido posible enviar el mensaje",
         })
     })
 }
